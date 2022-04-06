b973327fa490337e0c6b80485652c28416147bc1
Stiven_Ruiz <stiven70@outlook.com>
2022-04-06 11:29:25 -0500

    Se soluciona el error, faltaba el nombre de la columna en la tabla estructuras

--- database/migrations/2022_03_30_215104_create_estructuras_table.php
+++ database/migrations/2022_03_30_215104_create_estructuras_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('estructuras', function (Blueprint $table) {
             $table->id();
-            $table->string('');
+            $table->string('nombre');
             $table->timestamps();
         });
     }
